83e9b1859dcd130a94800efa1ca166312f20a018
Lenovo <Lenovo@DESKTOP-U8HCIH4>
2022-09-13 05:08:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Matrix.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrica
+{
+    public class Matrix
+    {
+        int a, b;
+        int[,] matrix;
+        public Matrix(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+            matrix = new int[a, b];
+            Random r = new Random();
+            for (int pr = 0; pr < this.a; pr++)
+            {
+                for (int ar = 0; ar < this.b; ar++)
+                {
+                    matrix[pr, ar] = r.Next(5, 10);
+                }
+            }
+        }
+
+        public void Sum(Matrix matrix)
+        {
+            if (this.matrix.Length <= matrix.matrix.Length)
+            {
+                for (int a = 0; a < this.a; a++)
+                {
+                    for (int b = 0; b < this.b; b++)
+                    {
+                        this.matrix[a, b] += matrix.matrix[a, b];
+                    }
+                }
+            }
+            else
+            {
+                for (int a = 0; a < matrix.a; a++)
+                {
+                    for (int n = 0; b < matrix.b; b++)
+                    {
+                        this.matrix[a, b] += matrix.matrix[a, b];
+                    }
+                }
+            }
+        }
+        public void Multiply(int k)
+        {
+            for (int a = 0; a < this.a; a++)
+            {
+                for (int b = 0; b < this.b; b++)
+                {
+                    matrix[a, b] *= k;
+                }
+            }
+        }
+        public void Print()
+        {
+            for (int a = 0; a < this.a; a++)
+            {
+                for (int b = 0; b < this.b; b++)
+                {
+                    Console.Write(this.matrix[a, b] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public void MatrixMatrix(Matrix matrix)
+        {
+            int[,] tempMatrix = new int[this.a, matrix.b];
+            for (int a = 0; a < this.a; a++)
+            {
+                for (int b = 0; b < matrix.a; b++)
+                {
+                    for (int k = 0; k < matrix.b; k++)
+                    {
+                        tempMatrix[a, b] += this.matrix[a, k] * matrix.matrix[k, b];
+                    }
+                }
+            }
+            this.matrix = tempMatrix;
+        }
+    }
+}
---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Matrica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrix1 = new Matrix(5, 5);
+            Matrix matrix2 = new Matrix(4, 4);
+            Matrix matrix3 = new Matrix(3, 3);
+            Matrix matrix4 = new Matrix(2, 2);
+
+            Console.WriteLine("Сумма первой и второй матрицы:");
+            matrix1.Sum(matrix2);
+            matrix1.Print();
+
+            Console.WriteLine("Умножение:");
+            matrix4.Multiply(7); 
+            matrix4.Print();
+            
+            Console.WriteLine("Умножение матрицы на матрицу:");
+            matrix2.MatrixMatrix(matrix3);
+            matrix2.Print();
+        }
+    }
+}
